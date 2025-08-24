606f71c3c3850b0b1580ddb7bedf4115f77879a1
jayti <jaytishah20@gmail.com>
2025-08-24 21:49:38 +0530

    added all files

---
+++ counter/index.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+function inc() {
+  count++;
+  document.getElementById('count').innerHTML = count;
+}
+
+function dec() {
+  count--;
+  document.getElementById('count').innerHTML = count;
+}
+
+function reset() {
+  count = 0;
+  document.getElementById('count').innerHTML = count;
+}
